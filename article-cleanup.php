03de6e141c541d01fbfd236f75c1d11644ed13a9
TJ Rogers <tj.rogers7@gmail.com>
2019-07-15 11:32:46 -0400

    - Add Contact SB Button to Blog section

--- article-cleanup.php
+++ article-cleanup.php
@@ -1,18 +1,7 @@
-<img class="wp-image-1434 size-full flt-right" src="http://www.sodandlandscaping.services.php72-37.lan3-1.websitetestlink.com/wp-content/uploads/2016/08/perennials.jpg" alt="perennials" width="350" height="233" />
-
-Spring is nearly here and what better way to celebrate it than hitting the gardens and installing some colorful perennials to brighten up the lawn. <strong>Perennial plants are the backbone of every landscaping garden project</strong>.
-
-Unlike annuals they die back to the groundeach year and resurface from their rootstock in the spring time. They generally go through a vegetative reproduction and mainly come in bulbs vs. annuals which will drop seeds to continue the life cycle.
-
-There are many different varieties to choose from. Some will attract butterflies and bees; others will simply add brilliant shades of blue, reds and yellows. Be careful when choosing the right bulbs for your garden, check to make sure they’ll thrive in your plant hardiness zone and what sunlight conditions they require here in Orlando and Central Florida.
-
-<h2>How To Landscape With Perennials</h2>
-Perennials can be short-lived (three years), and some may only flower once (monocarpic) but for the majority of perennials, you’ll be rewarded with a flowering cycle throughout their life (polycarpic). Some species of perennials will last year round; these are evergreen perennials.
-
-Others in temperate climates will grow and bloom during the warm season while losing its foliage in the colder months (deciduous). In some parts of the world, the cycle is determined by wet and dry seasons (tropical). <strong>Planting perennials outside of their hardiness zone can reduce their life span and turn them into annuals</strong>.
-
-If your soil is clay-like and heavy, it should be spaded and rototilled to a depth of 12”-18” then mix in some nutrients and organic material such as compost, peat moss, sand or other light material. Even if you have adequate drainage and quality soil, you’ll need to till up and prep the ground for installation to a depth of 12”.<img class="fullwidth-image-blog size-full wp-image-1325 aligncenter" src="http://www.sodandlandscaping.services/wp-content/uploads/2016/07/get-to-know.jpg" alt="perennial plant" width="360" height="203" />
-<h2>Perennial Plants Have Value</h2>
-The professional landscaper and avid gardener will form a healthy blend of trees, shrubs, perennials and annuals along with different shapes and size when designing a project. Trees and shrubs are main fixtures, <strong>whereas perennials provide the majority of the color you’ll see in a well put together a garden.</strong>
-
-Perennials range from coneflowers and daylilies to ornamental grasses. There're sun perennial and shade perennial options available, as well as, a wide range of shapes and sizes along with different colors. Go out there and spruce up your flower beds with a variety of perennials, landscaping your garden is the outdoor canvas, paint away. Remember the folks here at From the Ground Up Landscaping are here to help design and create flower beds or any other outdoor project no matter the size.
+<a class="btn" href="/contact">
+	<span class="contact-icon"><i class="fas fa-phone"></i></span>
+	<span class="contact-text">
+		<span class="contact-text-section contact">Contact</span>
+		<span class="contact-text-section us">Us</span>
+	</span>
+</a>
