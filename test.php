5d35782493cac4123ce755bb371fdfa8dc256f8d
TJ Rogers <tj.rogers7@gmail.com>
2019-07-24 15:44:26 -0400

    - Surfaces Landing page work - Layout

---
+++ test.php
@@ -0,0 +1,59 @@
+<div class="page-leadin">
+Everyone loves cream cheese camembert de normandie. Cheesy feet cheesy feet bocconcini manchego cheese on toast pepper jack manchego cheese strings. Cow cheesy feet cheese triangles taleggio swiss paneer goat cheese and wine. Roquefort camembert de normandie cut the cheese cauliflower cheese melted cheese gouda cheesy grin macaroni cheese. Caerphilly airedale manchego halloumi.</div>
+<div class="surfaces-landing-listing">
+
+<div class="surface lawns">
+<div class="surface-photo"><img src="http://sodandlandscaping.build/wp-content/uploads/2019/07/ph-surfaces-img.jpg" /></div>
+<div class="surface-content">
+<p class="surface-name">Lawns</p>
+<p class="surface-description">Short description to go here.</p>
+</div>
+<p class="surface-link-container"><a href="/surfaces/lawns/" class="surface-link">&nbsp;</a></p>
+</div>
+
+<div class="surface pets">
+<div class="surface-photo"><img src="http://sodandlandscaping.build/wp-content/uploads/2019/07/ph-surfaces-img-2.jpg" /></div>
+<div class="surface-content">
+<p class="surface-name">Pets</p>
+<p class="surface-description">Short description to go here.</p>
+</div>
+<p class="surface-link-container"><a href="/surfaces/pet/" class="surface-link">&nbsp;</a></p>
+</div>
+
+<div class="surface playgrounds">
+<div class="surface-photo"><img src="http://sodandlandscaping.build/wp-content/uploads/2019/07/ph-surfaces-img-3.jpg" /></div>
+<div class="surface-content">
+<p class="surface-name">Playgrounds</p>
+<p class="surface-description">Short description to go here.</p>
+</div>
+<p class="surface-link-container"><a href="/surfaces/playgrounds/" class="surface-link">&nbsp;</a></p>
+</div>
+
+<div class="surface putting">
+<div class="surface-photo"><img src="http://sodandlandscaping.build/wp-content/uploads/2019/07/ph-surfaces-img-2.jpg" /></div>
+<div class="surface-content">
+<p class="surface-name">Putting</p>
+<p class="surface-description">Short description to go here.</p>
+</div>
+<p class="surface-link-container"><a href="/surfaces/putting/" class="surface-link">&nbsp;</a></p>
+</div>
+
+<div class="surface pavers">
+<div class="surface-photo"><img src="http://sodandlandscaping.build/wp-content/uploads/2019/07/ph-surfaces-img-3.jpg" /></div>
+<div class="surface-content">
+<p class="surface-name">Pavers</p>
+<p class="surface-description">Short description to go here.</p>
+</div>
+<p class="surface-link-container"><a href="/surfaces/pavers/" class="surface-link">&nbsp;</a></p>
+</div>
+
+<div class="surface natural">
+<div class="surface-photo"><img src="http://sodandlandscaping.build/wp-content/uploads/2019/07/ph-surfaces-img.jpg" /></div>
+<div class="surface-content">
+<p class="surface-name">Natural</p>
+<p class="surface-description">Short description to go here.</p>
+</div>
+<p class="surface-link-container"><a href="/surfaces/natural-turf/" class="surface-link">&nbsp;</a></p>
+</div>
+
+</div>
