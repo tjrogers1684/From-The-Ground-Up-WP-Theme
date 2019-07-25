4ef6da2445f6286bff99632f12c0d63aa0a9124a
TJ Rogers <tj.rogers7@gmail.com>
2019-07-25 11:10:34 -0400

    - Work on styling and formatting Individual Surface pages

--- test.php
+++ test.php
@@ -1,3 +1,5 @@
+<?php // ------------------------------------------------ SURFACES LANDING PAGE --------------------------------------- ?>
+
 <div class="page-leadin">
 Everyone loves cream cheese camembert de normandie. Cheesy feet cheesy feet bocconcini manchego cheese on toast pepper jack manchego cheese strings. Cow cheesy feet cheese triangles taleggio swiss paneer goat cheese and wine. Roquefort camembert de normandie cut the cheese cauliflower cheese melted cheese gouda cheesy grin macaroni cheese. Caerphilly airedale manchego halloumi.</div>
 <div class="surfaces-landing-listing">
@@ -57,3 +59,101 @@ Everyone loves cream cheese camembert de normandie. Cheesy feet cheesy feet bocc
 </div>
 
 </div>
+
+
+<?php // ------------------------------------------------ INDY SURFACE PAGE --------------------------------------- ?>
+
+<!-- LEADIN SECTION  -->
+<div class="page-leadin">
+<p>From The Ground Up Landscaping is Central Florida’s go-to commercial and residential artificial grass installer and we are more than happy to help guide you through the process in choosing and installing the RIGHT synthetic turf for specific application.</p>
+
+<p>What separates us from other artificial turf companies in Central Florida is our unique ability to provide not one or two artificial grass manufacturers, but several manufacturers’ products. This allows us to provide you with an unparalleled selection of quality to meet your landscaping needs and budget!</p>
+</div>
+
+<!-- BENEFITS SECTION  -->
+<div class="surface-benefits-section">
+
+<div class="benefits-photo" style="background-image: url(/wp-content/themes/ftgu/images/artificial-lawn-residential.jpg);"></div>
+<div class="benefits-content">
+<h2>Turf Benefits</h2>
+<p class="benefits-tagline">Quality at every stage</p>
+<p>Easy maintenance. It's soft, itch-free, ALWAYS green; everything you could ever want in lawn!</p>
+<h3>Benefits</h3>
+<ul class="benefits-listing">
+	<li>Awesome</li>
+	<li>Awesome</li>
+	<li>Awesome</li>
+	<li>Awesome</li>
+	<li>Awesome</li>
+	<li>Awesome</li>
+	<li>Awesome</li>
+	<li>Awesome</li>
+</ul>
+<p class="product-get-started-link-container"><a href="/contact/" class="btn">Let's Get Started! <i class="fas fa-long-arrow-alt-right"></i></a></p>
+</div>
+</div>
+
+<!-- PRODUCT INFO SECTION  -->
+<div class="surface-product-section">
+	<div class="product-photo"><img src="/wp-content/themes/ftgu/images/artificial-lawn-residential.jpg" alt=""></div>
+
+	<div class="product-content">
+		<h2>Product Name</h2>
+
+		<p class="product-tagline">Cool tagline about product</p>
+
+		<p>Bacon ipsum dolor amet aliqua veniam in velit duis deserunt kielbasa exercitation jowl irure minim venison filet mignon. Pork belly commodo ex shoulder, voluptate jerky velit beef drumstick biltong. Meatloaf fugiat meatball culpa rump frankfurter labore dolore duis exercitation short loin. Landjaeger beef qui in sausage aliquip pancetta ground round short ribs voluptate tongue chicken.</p>
+
+		<p>In fatback flank, lorem irure pastrami short ribs tail officia consequat pork belly duis. Est short loin salami doner tongue elit ad pig ea ipsum. Ut chuck leberkas beef, pork chop beef ribs turducken landjaeger ribeye sunt labore culpa cillum non. Ut lorem shank leberkas, bresaola turducken meatball boudin laboris jerky. Ground round eu minim esse bacon fatback magna drumstick frankfurter nulla mollit tongue ham tri-tip.</p>
+
+		<p class="product-get-started-link-container"><a href="/contact/" class="btn">Let's Get Started! <i class="fas fa-long-arrow-alt-right"></i></a></p>
+	</div>
+</div>
+
+<!-- GALLERY SECTION  -->
+<div class="surface-gallery-section">
+	<h2>Product Gallery</h2>
+
+	<div class="surface-gallery">
+		<p class="surface-gallery-item"><a rel="lightbox" href="http://sodandlandscaping.build/wp-content/uploads/2019/07/ph-surfaces-img-3.jpg"><img src="http://sodandlandscaping.build/wp-content/uploads/2019/07/ph-surfaces-img-3.jpg" /></a></p>
+
+		<p class="surface-gallery-item"><a rel="lightbox" href="http://sodandlandscaping.build/wp-content/uploads/2019/07/ph-surfaces-img-2.jpg"><img src="http://sodandlandscaping.build/wp-content/uploads/2019/07/ph-surfaces-img-2.jpg" /></a></p>
+
+		<p class="surface-gallery-item"><a rel="lightbox" href="http://sodandlandscaping.build/wp-content/uploads/2019/07/ph-surfaces-img.jpg"><img src="http://sodandlandscaping.build/wp-content/uploads/2019/07/ph-surfaces-img.jpg" /></a></p>
+
+		<p class="surface-gallery-item"><a rel="lightbox" href="http://sodandlandscaping.build/wp-content/uploads/2019/07/ph-surfaces-img-3.jpg"><img src="http://sodandlandscaping.build/wp-content/uploads/2019/07/ph-surfaces-img-3.jpg" /></a></p>
+
+		<p class="surface-gallery-item"><a rel="lightbox" href="http://sodandlandscaping.build/wp-content/uploads/2019/07/ph-surfaces-img.jpg"><img src="http://sodandlandscaping.build/wp-content/uploads/2019/07/ph-surfaces-img.jpg" /></a></p>
+
+		<p class="surface-gallery-item"><a rel="lightbox" href="http://sodandlandscaping.build/wp-content/uploads/2019/07/ph-surfaces-img-3.jpg"><img src="http://sodandlandscaping.build/wp-content/uploads/2019/07/ph-surfaces-img-3.jpg" /></a></p>
+
+		<p class="surface-gallery-item"><a rel="lightbox" href="http://sodandlandscaping.build/wp-content/uploads/2019/07/ph-surfaces-img.jpg"><img src="http://sodandlandscaping.build/wp-content/uploads/2019/07/ph-surfaces-img.jpg" /></a></p>
+
+		<p class="surface-gallery-item"><a rel="lightbox" href="http://sodandlandscaping.build/wp-content/uploads/2019/07/ph-surfaces-img-2.jpg"><img src="http://sodandlandscaping.build/wp-content/uploads/2019/07/ph-surfaces-img-2.jpg" /></a></p>
+	</div>
+</div>
+
+<!-- CONTACT FORM SECTION  -->
+<div class="surface-contact-section">
+	<h2>Getting Started</h2>
+	<p>[ninja_form id=2]</p>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
